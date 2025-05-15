81641c50048d976c9edfba8358b145293d7c4338
Akeem Mudashiru <akeemmudash@gmail.com>
2025-05-15 13:54:43 +0100

    fix: modified to fix deploy errors

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,17 @@
+import { Link } from "react-router-dom";
+
 function Sidebar() {
   return (
     <div className="w-80 h-screen flex  py-4">
       <nav className="px-4 py-4">
-        <h2 className="text-4xl font-semibold">Sidebar</h2>
-        <ul>
-          <li>{/* <Link to="/home">Home</Link> */}</li>
-          <li>{/* <Link to="/countries">Countries</Link> */}</li>
+        <h2 className="text-4xl font-semibold mb-3">Sidebar</h2>
+        <ul className="space-y-4">
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+          <li>
+            <Link to="/">Countries</Link>
+          </li>
         </ul>
       </nav>
       <div className="gradient ms-auto border border-gray-100 h-full w-4 bg-stripe-pattern rounded-md"></div>
